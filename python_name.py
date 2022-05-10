d6fafdd022c4c1c8ef02295d3e5b3fa73fa07cc4
unknown <cjswhk45@naver.com>
2022-05-11 01:07:14 +0900

    Update_20220511

--- python_name.py
+++ python_name.py
@@ -1,6 +1,10 @@
 import os
+import sys
 
 target = r"C:\Users\cjswh\Desktop\정리자료"
 files = os.listdir(target)
+sys.stdout = open('name.csv','w')
 
-print("files.{}".format(files))
+print("files.{}".format(files), end='\n')
+
+sys.stdout.close()
